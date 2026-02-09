053261fca8ef58f8cdb63506a2997a7406ea2520
Khushi Patel <khushi108@icloud.com>
2026-02-09 15:51:23 -0500

    changing embeddings name

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ index = pc.Index(index_name)
 loader = CSVLoader(file_path="ME.csv")
 documents = loader.load()
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-001",
+    model="embedding-001",
     google_api_key=os.getenv("GEMINI_API_KEY")
 )
 
